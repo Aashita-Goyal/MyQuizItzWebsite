51e7b5ba3507a1bc52fe36d2c7d34559f35aae30
aparna <aparna19shah@gmail.com>
2022-04-07 22:18:00 +0530

    aparna is cool

---
+++ database/login.php
@@ -0,0 +1,15 @@
+<?php
+// $host='localhost';
+// $user='root';
+// $pass='';
+// $db='QuizItz';
+// $con=mysqli_connect($host,$user,$password,$db);
+// if($con)
+// echo 'connected succesfully to quizitz database';
+// $sql="insert into login(email,password) values('sakshit90@gmail.com','sakshilove')";
+// $query=mysqli_query($con,$sql);
+// if($query)
+echo 'data inserted succesfully';
+
+?>
+
