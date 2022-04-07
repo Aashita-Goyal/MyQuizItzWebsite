968ee08762fd0741aaf872e00c664aed076d7a49
aparna <aparna19shah@gmail.com>
2022-04-07 23:10:07 +0530

    index

--- database/login.php
+++ database/login.php
@@ -6,9 +6,9 @@
 // $con=mysqli_connect($host,$user,$password,$db);
 // if($con)
 // echo 'connected succesfully to quizitz database';
-// $sql="insert into login(email,password) values('sakshit90@gmail.com','sakshilove')";
-// $query=mysqli_query($con,$sql);
-// if($query)
+//  $sql="insert into login(email,password) values('sakshit90@gmail.com','sakshilove')";
+//  $query=mysqli_query($con,$sql);
+//  if($query)
 echo 'data inserted succesfully';
 
 ?>
