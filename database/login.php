32f54d514bb13d4b89a0d9678fbcd9be3c987e47
aparna <aparna19shah@gmail.com>
2022-04-08 13:48:01 +0530

    logout

--- database/login.php
+++ database/login.php
@@ -1,14 +1,14 @@
 <?php
-// $host='localhost';
-// $user='root';
-// $pass='';
-// $db='QuizItz';
-// $con=mysqli_connect($host,$user,$password,$db);
-// if($con)
-// echo 'connected succesfully to quizitz database';
-//  $sql="insert into login(email,password) values('sakshit90@gmail.com','sakshilove')";
-//  $query=mysqli_query($con,$sql);
-//  if($query)
+$host='localhost';
+$user='root';
+$pass='';
+$db='quizitz';
+$con=mysqli_connect($host,$user,$password,$db);
+if($con)
+echo 'connected succesfully to quizitz database';
+ $sql="insert into login(email,password) values('sakshit90@gmail.com','sakshilove')";
+ $query=mysqli_query($con,$sql);
+ if($query)
 echo 'data inserted succesfully';
 
 ?>
